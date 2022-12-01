fd3b426c38c98772173e9b5fb454a6a4a9a9b200
kdomo <dongho5088@naver.com>
2022-12-01 23:44:19 +0900

    Initial commit

---
+++ src/main/java/com/recodeit/server/RecodeItServerApplication.java
@@ -0,0 +1,13 @@
+package com.recodeit.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecodeItServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecodeItServerApplication.class, args);
+    }
+
+}
