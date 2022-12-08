fff44ecb8dca59e83c66490e112db660f55d939f
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-08 21:59:29 +0900

    [BE-15] feat: BaseEntity 정의

---
+++ src/main/java/com/recodeit/server/domain/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.recodeit.server.domain;
+
+import com.sun.istack.NotNull;
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @NotNull
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @NotNull
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    @NotNull
+    protected boolean isDeleted;
+}
