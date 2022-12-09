c13fccc8528f4f41d6416a9c69cfeadfbb6708b8
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-09 09:37:17 +0900

    [BE-12] feat: string으로 LoginType을 찾는 기능 추가

--- src/main/java/com/recodeit/server/constant/LoginType.java
+++ src/main/java/com/recodeit/server/constant/LoginType.java
@@ -1,7 +1,16 @@
 package com.recodeit.server.constant;
 
+import java.util.Arrays;
+
 public enum LoginType {
     LOCAL,
     KAKAO,
     GOOGLE;
+
+    public static LoginType findByString(String str) {
+        return Arrays.stream(LoginType.values())
+                .filter(loginType -> loginType.name().equals(str))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(""));
+    }
 }
