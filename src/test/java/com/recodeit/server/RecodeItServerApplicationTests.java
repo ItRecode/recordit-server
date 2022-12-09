f869695be93de1780a88955391042a3c13ae10ac
kdomo <dongho5088@naver.com>
2022-12-09 17:11:00 +0900

    [BE-14] fix: Profile을 ActiveProfiles로 변경

--- src/test/java/com/recodeit/server/RecodeItServerApplicationTests.java
+++ src/test/java/com/recodeit/server/RecodeItServerApplicationTests.java
@@ -3,9 +3,10 @@ package com.recodeit.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@Profile("test")
+@ActiveProfiles("test")
 class RecodeItServerApplicationTests {
 
     @Test
