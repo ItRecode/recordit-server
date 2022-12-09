6b2adba98ad6b119ccae30c5b417d47eaa4cf761
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-09 09:48:19 +0900

    [BE-12] feat: Transactional 어노테이션 추가

--- src/main/java/com/recodeit/server/service/MemberService.java
+++ src/main/java/com/recodeit/server/service/MemberService.java
@@ -4,16 +4,19 @@ import com.recodeit.server.service.oauth.OauthService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final List<OauthService> oauthServices;
 
+    @Transactional
     public void oauthLogin(String loginType) {
 
     }
 
+    @Transactional
     public void oauthRegister(String loginType) {
 
     }
