fe1529143534b9a81258490bc2a4cf53b2002a78
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-21 23:24:05 +0900

    [BE-59] feat: RecordCategory Entity 정의

---
+++ src/main/java/com/recordit/server/domain/RecordCategory.java
@@ -0,0 +1,36 @@
+package com.recordit.server.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "RECORD_CATEGORY")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Where(clause = "deleted_at is null")
+@SQLDelete(sql = "UPDATE RECORD_CATEGORY SET RECORD_CATEGORY.deletedAt = CURRENT_TIMESTAMP WHERE RECORD_CATEGORY.RECORD_CATEGORY_ID = ?")
+@Getter
+public class RecordCategory extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "RECORD_CATEGORY_ID")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "PARENT_RECORD_CATEGORY_ID")
+	private RecordCategory parentRecordCategory;
+
+	private String name;
+}
