79a6d4989b2fb554a11db83fb812583ff52e348c
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-22 12:20:31 +0900

    [BE-59] refactor: BaseEntity 컬럼명 대문자 스네이크로 변경

--- src/main/java/com/recordit/server/domain/BaseEntity.java
+++ src/main/java/com/recordit/server/domain/BaseEntity.java
@@ -2,6 +2,7 @@ package com.recordit.server.domain;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
@@ -19,11 +20,14 @@ public abstract class BaseEntity {
 
 	@NotNull
 	@CreatedDate
+	@Column(name = "CREATED_AT")
 	private LocalDateTime createdAt;
 
 	@NotNull
 	@LastModifiedDate
+	@Column(name = "MODIFIED_AT")
 	private LocalDateTime modifiedAt;
 
+	@Column(name = "DELETED_AT")
 	private LocalDateTime deletedAt;
 }
