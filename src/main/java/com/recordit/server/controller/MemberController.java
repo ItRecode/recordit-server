03bcd644092d727627a722970111457d846e9a86
kdomo <dongho5088@naver.com>
2022-12-22 15:05:58 +0900

    [BE-67] feat: 기존 API 명세 Swagger로 이전

--- src/main/java/com/recordit/server/controller/MemberController.java
+++ src/main/java/com/recordit/server/controller/MemberController.java
@@ -3,25 +3,46 @@ package com.recordit.server.controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.recordit.server.service.MemberService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
 
 	private final MemberService memberService;
 
-	@PostMapping("/member/oauth/{loginType}")
-	public void oauthLogin(@PathVariable("loginType") String loginType, @RequestBody String token) {
+	@ApiOperation(value = "로그인",
+			notes = "로그인 타입과 세션을 받아 가입된 회원이라면 로그인을 진행시키고 Header에 setCookie로 응답합니다")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "API 정상 작동 / Header에 setCookie값 응답"),
+			@ApiResponse(code = 401, message = "회원정보가 없어 회원가입이 필요한 경우입니다."
+					+ "Body로 응답된 TEMPSESSIONUUID와 사용자에게 닉네임을 받아 '/member/oauth/register/{loginType}'으로 요청하세요")
+	})
+	@PostMapping("/oauth/login/{loginType}")
+	public void oauthLogin(
+			@PathVariable("loginType") String loginType, @RequestBody String token) {
 		memberService.oauthLogin(loginType);
 	}
 
-	@PostMapping("/member/oauth/{loginType}")
-	public void oauthRegister(@PathVariable("loginType") String loginType) {
+	@ApiOperation(value = "회원가입",
+			notes = "로그인 타입, TEMPSESSIONID, 닉네임을 받아 회원가입을 진행합니다")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "API 정상 작동 / 세션 로그인 처리하고 SESSIONID 헤더로 응답"),
+			@ApiResponse(code = 428, message = "TEMPSESSIONUUID 정보가 Redis에 없거나 비정상적일 경우"),
+			@ApiResponse(code = 409, message = "닉네임이 중복 된 경우")
+	})
+
+	@PostMapping("/oauth/register/{loginType}")
+	public void oauthRegister(@PathVariable("loginType") String loginType, @RequestBody String nickname) {
 		memberService.oauthRegister(loginType);
 	}
 }
