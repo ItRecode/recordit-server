10dc5d2650e5fc81c120251b40d9d79b61df7ba5
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-22 21:45:00 +0900

    [BE-71] feat: 로그인/회원가입 관련 DTO 추가

---
+++ src/main/java/com/recordit/server/dto/member/RegisterSessionResponseDto.java
@@ -0,0 +1,28 @@
+package com.recordit.server.dto.member;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@ApiModel
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class RegisterSessionResponseDto {
+
+	@ApiModelProperty(notes = "회원가입시 필요한 임시 Session", required = true)
+	private String registerSession;
+
+	@Builder
+	public RegisterSessionResponseDto(String registerSession) {
+		this.registerSession = registerSession;
+	}
+}
