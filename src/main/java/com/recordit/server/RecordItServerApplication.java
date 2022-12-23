e00d8187c1fe5921c3ff3a8ecf0d6604370671a1
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-23 20:53:20 +0900

    [BE-72] feat: ConfigurationProperties을 사용해 Google, Kakao Oauth 인증 관련 환경변수 받는 기능 추가

--- src/main/java/com/recordit/server/RecordItServerApplication.java
+++ src/main/java/com/recordit/server/RecordItServerApplication.java
@@ -2,8 +2,12 @@ package com.recordit.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class RecordItServerApplication {
 
 	public static void main(String[] args) {
