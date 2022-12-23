0531458d4e64bc13fad772cb9897c149d3194566
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-23 13:08:33 +0900

    [BE-29] feat: RedisExceptionHandler와 Parsing 예외 추가

---
+++ src/main/java/com/recordit/server/exception/redis/RedisExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.recordit.server.exception.redis;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.recordit.server.exception.ErrorMessage;
+import com.recordit.server.exception.member.NotFoundUserInfoInSessionException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class RedisExceptionHandler {
+
+	@ExceptionHandler(RedisParsingException.class)
+	public ResponseEntity<ErrorMessage> handleRedisParsingException(
+			NotFoundUserInfoInSessionException exception) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(ErrorMessage.of(exception, HttpStatus.INTERNAL_SERVER_ERROR));
+	}
+
+}
