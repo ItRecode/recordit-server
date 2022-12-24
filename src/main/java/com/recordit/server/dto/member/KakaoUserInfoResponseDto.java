7be233f51d5ae1b1a9ac7547c9350256b1cd4a2f
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-24 16:28:25 +0900

    [BE-72] feat: KakaoUserInfoResponseDto에 JsonIgnoreProperties 어노테이션을 통해 필드값만 Parsing하도록 설정

--- src/main/java/com/recordit/server/dto/member/KakaoUserInfoResponseDto.java
+++ src/main/java/com/recordit/server/dto/member/KakaoUserInfoResponseDto.java
@@ -1,5 +1,6 @@
 package com.recordit.server.dto.member;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -12,6 +13,7 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoUserInfoResponseDto {
 	private String id;
 	private String connectedAt;
