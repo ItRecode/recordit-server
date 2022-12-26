adc0e21fbd9b3e1c09e00a0c5a16625f1ce673b1
kdomo <dongho5088@naver.com>
2022-12-26 13:35:14 +0900

    [BE-77] fix: Builder 위치 변경

--- src/main/java/com/recordit/server/dto/record/category/RecordCategoryResponseDto.java
+++ src/main/java/com/recordit/server/dto/record/category/RecordCategoryResponseDto.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,9 +19,12 @@ import lombok.ToString;
 @ApiModel
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-@AllArgsConstructor
-@Builder
 public class RecordCategoryResponseDto {
 	@ApiModelProperty(notes = "레코드 카테고리 목록", required = true)
 	private Map<String, List<String>> recordCategory;
+
+	@Builder
+	public RecordCategoryResponseDto(Map<String, List<String>> recordCategory) {
+		this.recordCategory = recordCategory;
+	}
 }
