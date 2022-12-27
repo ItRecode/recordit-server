0876be5ef87599c048bafdf4ed56e4fd81aeed80
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-28 01:12:10 +0900

    [BE-85] feat: S3 업로드 및 URL을 받아오는 S3Uploader 추가

---
+++ src/main/java/com/recordit/server/util/S3Uploader.java
@@ -0,0 +1,54 @@
+package com.recordit.server.util;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.recordit.server.environment.S3Properties;
+import com.recordit.server.exception.imagefile.FileInputStreamException;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class S3Uploader {
+
+	private final AmazonS3 amazonS3;
+	private final S3Properties s3Properties;
+
+	public String upload(@NonNull MultipartFile multipartFile) {
+		String fileName = UUID.randomUUID().toString();
+		try {
+			amazonS3.putObject(
+					new PutObjectRequest(
+							s3Properties.getBucket(),
+							fileName,
+							multipartFile.getInputStream(),
+							getObjectMetadataBy(multipartFile)
+					).withCannedAcl(CannedAccessControlList.PublicRead)
+			);
+		} catch (IOException e) {
+			throw new FileInputStreamException("해당 파일을 읽어올 수 없습니다.");
+		}
+		return fileName;
+	}
+
+	public String getUrlByFileName(String fileName) {
+		return amazonS3.getUrl(s3Properties.getBucket(), fileName).toString();
+	}
+
+	private ObjectMetadata getObjectMetadataBy(@NonNull MultipartFile multipartFile) {
+		ObjectMetadata objectMetadata = new ObjectMetadata();
+		objectMetadata.setContentLength(multipartFile.getSize());
+		objectMetadata.setContentType(multipartFile.getContentType());
+		return objectMetadata;
+	}
+
+}
