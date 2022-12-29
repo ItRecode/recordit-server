c2332702b365c392789f901f0e34b9cc6887a247
kdomo <dongho5088@naver.com>
2022-12-29 12:37:02 +0900

    [BE-30] fix: 테스트 결과에 따른 fetch join 적용해제

--- src/main/java/com/recordit/server/repository/RecordRepository.java
+++ src/main/java/com/recordit/server/repository/RecordRepository.java
@@ -3,12 +3,11 @@ package com.recordit.server.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.recordit.server.domain.Record;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
-	@Query("select r from RECORD r join fetch r.writer join fetch r.recordColor join fetch r.recordIcon"
-			+ " where r.id = :id")
+	// @Query("select r from RECORD r join fetch r.writer join fetch r.recordColor join fetch r.recordIcon"
+	// 		+ " where r.id = :id")
 	Optional<Record> findById(Long id);
 }
