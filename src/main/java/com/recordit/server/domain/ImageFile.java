1c85985e7318fcde5dd54fe9a81b4d8e1f9e937b
Jaeyeop-Jung <jjy5349@naver.com>
2022-12-29 17:30:14 +0900

    [BE-93] refactor: 유니크키 삭제 및 인덱스 설정, 정적 팩토리 메소드 추가, 파일 사이즈 타입을 Long으로 변경

--- src/main/java/com/recordit/server/domain/ImageFile.java
+++ src/main/java/com/recordit/server/domain/ImageFile.java
@@ -7,11 +7,12 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.recordit.server.constant.RefType;
 
@@ -20,9 +21,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity(name = "IMAGE_FILE")
-@Table(uniqueConstraints = {
-		@UniqueConstraint(columnNames = {"REF_TYPE", "REF_ID"})
-})
+@Table(indexes = @Index(columnList = "REF_TYPE, REF_ID"))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Where(clause = "DELETED_AT is null")
 @SQLDelete(sql = "UPDATE IMAGE_FILE SET IMAGE_FILE.DELETED_AT = CURRENT_TIMESTAMP WHERE IMAGE_FILE.IMAGE_FILE_ID = ?")
@@ -54,6 +53,41 @@ public class ImageFile extends BaseEntity {
 	private String extension;
 
 	@Column(name = "SIZE")
-	private Integer size;
+	private Long size;
 
+	private ImageFile(
+			RefType refType,
+			Long refId,
+			String downloadUrl,
+			String originalName,
+			String saveName,
+			String extension,
+			Long size
+	) {
+		this.refType = refType;
+		this.refId = refId;
+		this.downloadUrl = downloadUrl;
+		this.originalName = originalName;
+		this.saveName = saveName;
+		this.extension = extension;
+		this.size = size;
+	}
+
+	public static ImageFile of(
+			RefType refType,
+			Long refId,
+			String saveUrl,
+			String saveName,
+			MultipartFile multipartFile
+	) {
+		return new ImageFile(
+				refType,
+				refId,
+				saveUrl,
+				multipartFile.getOriginalFilename(),
+				saveName,
+				multipartFile.getOriginalFilename().substring(multipartFile.getOriginalFilename().lastIndexOf(".") + 1),
+				multipartFile.getSize()
+		);
+	}
 }
