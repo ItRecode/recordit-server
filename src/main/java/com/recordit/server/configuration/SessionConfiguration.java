000ac1ddf4077e941a53233715861901de97cdf4
Jaeyeop-Jung <jjy5349@naver.com>
2023-01-03 23:12:40 +0900

    test

--- src/main/java/com/recordit/server/configuration/SessionConfiguration.java
+++ src/main/java/com/recordit/server/configuration/SessionConfiguration.java
@@ -1,18 +1,28 @@
-// package com.recordit.server.configuration;
-//
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.session.web.http.CookieSerializer;
-// import org.springframework.session.web.http.DefaultCookieSerializer;
-//
-// @Configuration
-// public class SessionConfiguration {
-//
-// 	@Bean
-// 	public CookieSerializer cookieSerializer() {
-// 		DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-// 		serializer.setUseSecureCookie(true);
-// 		serializer.setSameSite("None");
-// 		return serializer;
-// 	}
-// }
+package com.recordit.server.configuration;
+
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SessionConfiguration {
+
+	@Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+		return new WebServerFactoryCustomizer<TomcatServletWebServerFactory>() {
+			@Override
+			public void customize(TomcatServletWebServerFactory tomcatServletWebServerFactory) {
+				tomcatServletWebServerFactory.addContextCustomizers(new TomcatContextCustomizer() {
+					@Override
+					public void customize(Context context) {
+						context.setCookieProcessor(new LegacyCookieProcessor());
+					}
+				});
+			}
+		};
+	}
+}
