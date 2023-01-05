77dc4a6e1a77a1073981542a89729d774a7e8e16
kdomo <dongho5088@naver.com>
2023-01-06 01:08:52 +0900

    [BE-134] fix: EnableCaching 어노테이션 누락 버그

--- src/main/java/com/recordit/server/RecordItServerApplication.java
+++ src/main/java/com/recordit/server/RecordItServerApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
+@EnableCaching
 public class RecordItServerApplication {
 
 	public static void main(String[] args) {
