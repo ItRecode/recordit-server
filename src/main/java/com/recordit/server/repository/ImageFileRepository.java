549f1e7a4da18a0ac96f1717f9e54a05592f6ef1
Jaeyeop-Jung <jjy5349@naver.com>
2023-01-09 20:42:10 +0900

    [BE-141] feat: 조건절을 넣은 쿼리메소드 추가

--- src/main/java/com/recordit/server/repository/ImageFileRepository.java
+++ src/main/java/com/recordit/server/repository/ImageFileRepository.java
@@ -1,8 +1,16 @@
 package com.recordit.server.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.recordit.server.constant.RefType;
 import com.recordit.server.domain.ImageFile;
 
 public interface ImageFileRepository extends JpaRepository<ImageFile, Long> {
+
+	Optional<ImageFile> findByRefTypeAndRefId(RefType refType, Long refId);
+
+	List<ImageFile> findAllByRefTypeAndRefId(RefType refType, Long refId);
 }
