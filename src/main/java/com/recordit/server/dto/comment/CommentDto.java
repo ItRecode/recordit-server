ca7a3f1ddce5164ff75e47d7d1059d19223171c9
Jaeyeop-Jung <jjy5349@naver.com>
2023-01-12 14:09:15 +0900

    [BE-130] feat: 댓글 조회 관련 DTO 내용 수정

--- src/main/java/com/recordit/server/dto/comment/CommentDto.java
+++ src/main/java/com/recordit/server/dto/comment/CommentDto.java
@@ -1,10 +1,15 @@
 package com.recordit.server.dto.comment;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.recordit.server.domain.Comment;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -15,9 +20,32 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CommentDto {
+
+	@ApiModelProperty(notes = "댓글 ID", required = true)
 	private Long commentId;
 
+	@ApiModelProperty(notes = "댓글 작성자", required = true)
+	private String writer;
+
+	@ApiModelProperty(notes = "댓글 내용", required = true)
 	private String content;
 
+	@ApiModelProperty(notes = "첨부 이미지 URL", required = true)
 	private String imageUrl;
+
+	@ApiModelProperty(notes = "생성 일자")
+	private LocalDateTime createdAt;
+
+	@ApiModelProperty(notes = "수정 일자")
+	private LocalDateTime modifiedAt;
+
+	@Builder
+	public CommentDto(Comment comment, String imageUrl) {
+		this.commentId = comment.getId();
+		this.writer = comment.getWriter().getNickname();
+		this.content = comment.getContent();
+		this.imageUrl = imageUrl;
+		this.createdAt = comment.getCreatedAt();
+		this.modifiedAt = comment.getModifiedAt();
+	}
 }
--- src/main/java/com/recordit/server/dto/comment/CommentResponseDto.java
+++ src/main/java/com/recordit/server/dto/comment/CommentResponseDto.java
@@ -1,15 +1,18 @@
 package com.recordit.server.dto.comment;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.recordit.server.domain.Comment;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -20,16 +23,28 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CommentResponseDto {
-	@ApiModelProperty(notes = "레코드의 id", required = true)
-	@NotNull
-	private Long recordId;
 
-	@ApiModelProperty(notes = "자식 댓글의 리스트일 경우 부모 댓글이 들어감")
-	private Long parentId;
+	@ApiModelProperty(notes = "요청 댓글의 전체 페이지 개수", required = true)
+	private Integer totalPage;
 
-	@ApiModelProperty(notes = "레코드 댓글의 전체 개수", required = true)
+	@ApiModelProperty(notes = "요청 댓글의 전체 개수", required = true)
 	private Long totalCount;
 
 	@ApiModelProperty(notes = "댓글 리스트", required = true)
 	private List<CommentDto> commentList;
+
+	@Builder
+	public CommentResponseDto(Page<Comment> comments, List<String> imageFileUrls) {
+		this.totalPage = comments.getTotalPages();
+		this.totalCount = comments.getTotalElements();
+		this.commentList = new ArrayList<>();
+		for (int i = 0; i < comments.getContent().size(); i++) {
+			commentList.add(
+					CommentDto.builder()
+							.comment(comments.getContent().get(i))
+							.imageUrl(imageFileUrls.get(i))
+							.build()
+			);
+		}
+	}
 }
