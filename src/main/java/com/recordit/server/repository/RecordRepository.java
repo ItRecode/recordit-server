58ab4af5281dd5264566eaf32010308933573dcd
Jaeyeop-Jung <jjy5349@naver.com>
2023-01-23 02:46:46 +0900

    [BE-148] refactor: 사용하지 않는 쿼리 삭제 및 작성자가 지정한 날짜에 작성한 레코드들을 조회하는 쿼리 추가

--- src/main/java/com/recordit/server/repository/RecordRepository.java
+++ src/main/java/com/recordit/server/repository/RecordRepository.java
@@ -3,10 +3,13 @@ package com.recordit.server.repository;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.recordit.server.domain.Member;
 import com.recordit.server.domain.Record;
@@ -16,10 +19,20 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
 	// 		+ " where r.id = :id")
 	// Optional<Record> findById(Long id);
 
-	Optional<Record> findTopByWriterAndCreatedAtBetweenOrderByCreatedAtDesc(
-			Member writer,
-			LocalDateTime startTime,
-			LocalDateTime endTime
+	@EntityGraph(attributePaths = {"recordCategory", "recordIcon", "recordColor"})
+	@Query("select r "
+			+ "from RECORD r "
+			+ "left join r.recordCategory "
+			+ "left join r.recordIcon "
+			+ "left join r.recordColor "
+			+ "where r.writer = :writer "
+			+ "and :startTime <= r.createdAt and r.createdAt <= :endTime "
+	)
+	Page<Record> findAllByWriterAndCreatedAtBetweenOrderByCreatedAtDesc(
+			@Param("writer") Member writer,
+			@Param("startTime") LocalDateTime startTime,
+			@Param("endTime") LocalDateTime endTime,
+			Pageable pageable
 	);
 
 	Slice<Record> findByWriterAndCreatedAtBefore(
