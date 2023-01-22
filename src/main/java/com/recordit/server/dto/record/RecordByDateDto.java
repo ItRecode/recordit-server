38b39fab6774877bce2e34105aeb426f43544781
Jaeyeop-Jung <jjy5349@naver.com>
2023-01-23 02:46:08 +0900

    [BE-148] refactor: 날짜로 레코드 조회할 때 사용되는 DTO 이름 변경 및 필드 수정

--- src/main/java/com/recordit/server/dto/record/RecordByDateDto.java
+++ src/main/java/com/recordit/server/dto/record/RecordByDateDto.java
@@ -16,21 +16,15 @@ import lombok.ToString;
 @ToString
 @ApiModel
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class TodayWriteRecordDto {
-	@ApiModelProperty(notes = "레코드 아이디")
-	private Long recordId;
-
-	@ApiModelProperty(notes = "레코드 작성 시각")
-	private LocalDateTime createdAt;
-
+public class RecordByDateDto {
 	@ApiModelProperty(notes = "레코드의 카테고리 이름")
 	private String categoryName;
 
+	@ApiModelProperty(notes = "레코드 아이디")
+	private Long recordId;
+
 	@ApiModelProperty(notes = "레코드의 제목")
 	private String title;
-
-	@ApiModelProperty(notes = "레코드에 달린 댓글 갯수")
-	private Long commentCount;
 
 	@ApiModelProperty(notes = "레코드의 아이콘 이름")
 	private String iconName;
@@ -38,8 +32,14 @@ public class TodayWriteRecordDto {
 	@ApiModelProperty(notes = "레코드의 색상 이름")
 	private String colorName;
 
+	@ApiModelProperty(notes = "레코드 작성 시각")
+	private LocalDateTime createdAt;
+
+	@ApiModelProperty(notes = "레코드에 달린 댓글 갯수")
+	private Long commentCount;
+
 	@Builder
-	public TodayWriteRecordDto(
+	public RecordByDateDto(
 			Record record,
 			Long commentCount
 	) {
