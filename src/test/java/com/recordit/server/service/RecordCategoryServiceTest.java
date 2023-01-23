58ffdb20ad50b8cbb438448537d89815fca46fc7
kdomo <dongho5088@naver.com>
2023-01-24 02:24:14 +0900

    [BE-111] test: 레코드 카테고리 저장 테스트

--- src/test/java/com/recordit/server/service/RecordCategoryServiceTest.java
+++ src/test/java/com/recordit/server/service/RecordCategoryServiceTest.java
@@ -4,16 +4,21 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.recordit.server.domain.RecordCategory;
 import com.recordit.server.dto.record.category.RecordCategoryResponseDto;
+import com.recordit.server.dto.record.category.SaveRecordCategoryRequestDto;
+import com.recordit.server.exception.record.category.RecordCategoryNotFoundException;
 import com.recordit.server.repository.RecordCategoryRepository;
 
 @ExtendWith({MockitoExtension.class})
@@ -62,4 +67,47 @@ class RecordCategoryServiceTest {
 		assertThat(result.get(1).getId()).isEqualTo(3L);
 	}
 
+	@Nested
+	@DisplayName("레코드 카테고리를 저장_할 때")
+	class 레코드_카테고리를_저장_할_때 {
+
+		@Test
+		@DisplayName("부모 카테고리를 찾을 수 없는 경우 예외를 던진다")
+		void 부모_카테고리를_찾을_수_없는_경우_예외를_던진다() {
+			// given
+			SaveRecordCategoryRequestDto saveRecordCategoryRequestDto = SaveRecordCategoryRequestDto.builder()
+					.parentCategoryId(2L)
+					.name("슬퍼요")
+					.build();
+
+			given(recordCategoryRepository.findById(anyLong()))
+					.willReturn(Optional.empty());
+
+			// when, then
+			assertThatThrownBy(() -> recordCategoryService.saveRecordCategory(saveRecordCategoryRequestDto))
+					.isInstanceOf(RecordCategoryNotFoundException.class)
+					.hasMessage("지정한 부모 카테고리 정보를 찾을 수 없습니다.");
+		}
+
+		@Test
+		@DisplayName("정상적이라면 예외를 던지지 않는다")
+		void 정상적이라면_예외를_던지지_않는다() {
+			// given
+			RecordCategory recordCategory1 = mock(RecordCategory.class);
+			SaveRecordCategoryRequestDto saveRecordCategoryRequestDto = SaveRecordCategoryRequestDto.builder()
+					.parentCategoryId(2L)
+					.name("슬퍼요")
+					.build();
+
+			given(recordCategoryRepository.findById(anyLong()))
+					.willReturn(Optional.of(recordCategory1));
+
+			ArgumentCaptor<RecordCategory> captor = ArgumentCaptor.forClass(RecordCategory.class);
+
+			// when, then
+			assertThatCode(() -> recordCategoryService.saveRecordCategory(saveRecordCategoryRequestDto))
+					.doesNotThrowAnyException();
+			verify(recordCategoryRepository, times(1)).save(captor.capture());
+		}
+	}
 }
