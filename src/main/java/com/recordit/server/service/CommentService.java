fa85cf9961f7444f37702edcc236a7aba127c060
kdomo <dongho5088@naver.com>
2023-01-28 11:38:02 +0900

    [BE-181] test, fix: 댓글 삭제 시 익명 댓글을 레코드 작성자가 삭제 가능하도록 변경

--- src/main/java/com/recordit/server/service/CommentService.java
+++ src/main/java/com/recordit/server/service/CommentService.java
@@ -17,6 +17,7 @@ import com.recordit.server.domain.Member;
 import com.recordit.server.domain.Record;
 import com.recordit.server.dto.comment.CommentRequestDto;
 import com.recordit.server.dto.comment.CommentResponseDto;
+import com.recordit.server.dto.comment.DeleteCommentRequestDto;
 import com.recordit.server.dto.comment.ModifyCommentRequestDto;
 import com.recordit.server.dto.comment.WriteCommentRequestDto;
 import com.recordit.server.dto.comment.WriteCommentResponseDto;
@@ -121,7 +122,7 @@ public class CommentService {
 	}
 
 	@Transactional
-	public void deleteComment(Long commentId) {
+	public void deleteComment(Long commentId, DeleteCommentRequestDto deleteCommentRequestDto) {
 		Long userIdBySession = sessionUtil.findUserIdBySession();
 		log.info("세션에서 찾은 사용자 ID : {}", userIdBySession);
 
@@ -131,11 +132,22 @@ public class CommentService {
 		Comment findComment = commentRepository.findById(commentId)
 				.orElseThrow(() -> new CommentNotFoundException("댓글 정보를 가져올 수 없습니다."));
 
-		if (findComment.getWriter().getId() != member.getId()) {
-			throw new NotMatchCommentWriterException("로그인한 사용자와 댓글 작성자가 일치하지 않습니다.");
-		}
-
+		validateDeleteCommentMatchMember(deleteCommentRequestDto.getRecordId(), member, findComment);
 		commentRepository.delete(findComment);
+	}
+
+	private void validateDeleteCommentMatchMember(
+			Long recordId,
+			Member member,
+			Comment comment
+	) {
+		if (comment.getWriter() == null || comment.getWriter().getId() != member.getId()) {
+			Record record = recordRepository.findByIdFetchWriter(recordId)
+					.orElseThrow(() -> new RecordNotFoundException("레코드 정보를 찾을 수 없습니다."));
+			if (record.getWriter().getId() != member.getId()) {
+				throw new NotMatchCommentWriterException("로그인한 사용자가 댓글 작성자 또는 레코드 작성자가 아닙니다.");
+			}
+		}
 	}
 
 	private void validateEmptyContent(
