21f93812feb37506c0699e16abe8a03632e2e60b
이재원 <jaewon971111@gmail.com>
2023-02-04 22:20:19 +0900

    [BE-196] fix : 누락된 파일 커밋

---
+++ src/main/java/com/recordit/server/util/MixRecordRandomUtil.java
@@ -0,0 +1,33 @@
+package com.recordit.server.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.recordit.server.domain.Comment;
+
+public class MixRecordRandomUtil {
+	private static Random random = new Random();
+
+	public static List<Comment> getRandomCommentList(List<Comment> commentList, Integer commentListSize) {
+		List<Comment> randomCommentList = new ArrayList<>();
+
+		if (commentList.size() == 0) {
+			return randomCommentList;
+		}
+
+		for (int i = 0; i < commentListSize; i++) {
+			randomCommentList.add(commentList.get(random.nextInt(commentList.size())));
+		}
+
+		return randomCommentList;
+	}
+
+	/*public static List<Record> getRandomRecordList(List<Record> recordList, Long recordListSize) {
+		List<Record> randomRecordList = new ArrayList<>();
+		for (int i = 0; i < recordListSize; i++) {
+			randomRecordList.add(recordList.get(random.nextInt(recordList.size())));
+		}
+		return randomRecordList;
+	}*/
+}
