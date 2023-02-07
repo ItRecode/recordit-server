b8ef95183de3cfc9d7c7b7edcdd967fff530cb45
Jaeyeop-Jung <jjy5349@naver.com>
2023-02-07 23:01:39 +0900

    [BE-195] refactor: 전체 카테고리 조회 API에서 상위 카테고리를 통해 서브 카테고리를 조회하도록 변경

--- src/main/java/com/recordit/server/controller/RecordCategoryController.java
+++ src/main/java/com/recordit/server/controller/RecordCategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.recordit.server.dto.record.category.RecordCategoryResponseDto;
@@ -15,6 +16,7 @@ import com.recordit.server.dto.record.category.SaveRecordCategoryRequestDto;
 import com.recordit.server.service.RecordCategoryService;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -27,8 +29,10 @@ public class RecordCategoryController {
 	private final RecordCategoryService recordCategoryService;
 
 	@ApiOperation(
-			value = "레코드 카테고리 전체 조회",
-			notes = "레코드 카테고리 전체를 조회합니다."
+			value = "레코드 카테고리 조회",
+			notes = "레코드 카테고리를 조회합니다. \t\n"
+					+ "부모 카테고리를 지정하지 않으면 상위 카테고리를 반환하고, \t\n"
+					+ "부모 카테고리를 지정하면 해당 부모의 하위 카테고리를 반환합니다."
 	)
 	@ApiResponses({
 			@ApiResponse(
@@ -37,8 +41,10 @@ public class RecordCategoryController {
 			)
 	})
 	@GetMapping
-	public ResponseEntity<List<RecordCategoryResponseDto>> getAllRecordCategories() {
-		return ResponseEntity.ok(recordCategoryService.getAllRecordCategories());
+	public ResponseEntity<List<RecordCategoryResponseDto>> getAllRecordCategories(
+			@ApiParam(value = "조회할 카테고리의 부모 ID") @RequestParam(required = false) Long parentRecordCategoryId
+	) {
+		return ResponseEntity.ok(recordCategoryService.getSubCategories(parentRecordCategoryId));
 	}
 
 	@PostMapping
