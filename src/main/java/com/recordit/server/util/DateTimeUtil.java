5651a59fb915c89d6011e18624582135aa25dc96
Jaeyeop-Jung <jjy5349@naver.com>
2023-02-13 22:40:20 +0900

    [BE-200] feat: RankingPeriod를 통해 해당 기간 이전의 LocalDateTime을 구하는 유틸 기능 정의

--- src/main/java/com/recordit/server/util/DateTimeUtil.java
+++ src/main/java/com/recordit/server/util/DateTimeUtil.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import com.recordit.server.constant.RankingPeriod;
+
 public class DateTimeUtil {
 
 	public static LocalDateTime getStartOfToday() {
@@ -16,5 +18,17 @@ public class DateTimeUtil {
 
 	public static LocalDateTime getEndOfDay(LocalDate localDate) {
 		return LocalDateTime.of(localDate, LocalTime.MAX);
+	}
+
+	public static LocalDateTime getBeforePeriodFromNow(RankingPeriod rankingPeriod) {
+		LocalDateTime now = LocalDateTime.now();
+		if (rankingPeriod == RankingPeriod.DAY) {
+			return now.minusDays(1);
+		} else if (rankingPeriod == RankingPeriod.WEEK) {
+			return now.minusWeeks(1);
+		} else if (rankingPeriod == RankingPeriod.MONTH) {
+			return now.minusMonths(1);
+		}
+		return LocalDateTime.MIN;
 	}
 }
