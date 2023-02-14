70ebaa070a6ca33e0316a63aca945ee7dc0f21a5
이재원 <jaewon971111@gmail.com>
2023-02-14 18:18:46 +0900

    [BE-210] test : 회원의 닉네임을 변경하는 API 테스트코드 작성

--- src/main/java/com/recordit/server/dto/member/ModifyMemberRequestDto.java
+++ src/main/java/com/recordit/server/dto/member/ModifyMemberRequestDto.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Pattern;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -19,4 +20,9 @@ public class ModifyMemberRequestDto {
 	@NotBlank(message = "변경할 닉네임은 빈값일 수 없습니다.")
 	@Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,8}$", message = "닉네임은 국문,영문,숫자를 포함한 2글자이상 8글자 이하입니다.")
 	private String nickName;
+
+	@Builder
+	public ModifyMemberRequestDto(String nickName) {
+		this.nickName = nickName;
+	}
 }
